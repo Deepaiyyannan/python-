c9e94da3b69f07a9767ef1d8256d9b98f904ada9
Deepaiyyannan <169175062+Deepaiyyannan@users.noreply.github.com>
2025-09-26 15:05:08 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,17 @@
+"""mark = int(input())
+if(mark>35):
+    print("Pass")
+else:
+    print("Fail")
+income = int(input())
+if(income<=7000):
+    print("eligil")
+else:
+    print(" not eligilbe")"""
+
+
+a= int(input())
+if(a%3==0 & a%5==0):
+    print("yes")
+else:
+    print("NO")
