5f8fb7f7148c07996a63a664f36c3eab49f3bfd1
Deepaiyyannan <169175062+Deepaiyyannan@users.noreply.github.com>
2025-09-26 14:42:15 +0530

    Add files via upload
    for loop basics

---
+++ for loop.py
@@ -0,0 +1,19 @@
+'''for i in "apple":
+    print(i)
+    
+for i in range(6):
+    print(i)
+for j in range(1,9):
+    print(j)
+for j in range(1,11):
+    print(j,"X2=",j*2)
+a=int(input())
+b=int(input())
+for j in range(a+1,b):
+    print(j)'''
+count=0
+for j in range(1,5):
+    if(j%2==0):
+        count=count+1
+        print(count)
+  
