ed41dc14aca37372fee32c9d3daab047772eaab6
Deepaiyyannan <169175062+Deepaiyyannan@users.noreply.github.com>
2025-09-26 15:04:49 +0530

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,33 @@
+"""a="10"
+b="20"
+c=int(a+b)
+print(c)
+
+name=str(input())
+age = int(input())
+address=str(input())
+print("My name is :" , name)
+print("My age is :", age)
+print("My address is : " , address)
+
+a=5
+b=8
+c=4
+add = a+b+c
+mul = a*b*c
+div = mul / add
+print(add)
+print(mul)
+print(div)
+
+name = str(input())
+score= int(input())
+dept = str(input())
+div = score / 10
+print(div)"""
+
+meghna = str(input())
+if(meghna == "Died"):
+    print("Surya meets Priya")
+else:
+    print("Surya weds meghna")
